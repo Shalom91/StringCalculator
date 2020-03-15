d64baec206f96ca5decda082eba64183c0b0d4e5
Shalom91 <56162445+Shalom91@users.noreply.github.com>
2020-03-15 11:09:23 +0200

    Update calculator.py

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,21 +2,13 @@
 import re
 
 def add(text): 
-    # split items and find integers: pattern, num_list
     pattern = re.split("4|\n|a-z|777", text)
     x = r"\-*\d+"
     num_list = re.findall(x, str(pattern))
-
-    # convert list items into int
     num_list = [int(i) for i in num_list]
-
-    # initialise placeholder for adding integers: addition
     addition = 0
-
-    # add items in a list
     for i in num_list:
         if i < 0: 
-            # use map() to convert each item in the list to a string, and then join them
             raise Exception("ERROR: Negatives not allowed " + ",".join(map(str, [i for i in num_list if i < 0])))
         elif i >= 1000: 
             pass
